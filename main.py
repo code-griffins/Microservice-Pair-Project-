476806a9261079f4e6f25bd98fc28712086c94df
Griffins Miruka <05mirukagriffins@gmail.com>
2025-09-24 01:38:36 -0400

    update README and modify home route to return JSON response

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-from flask import Flask
-
+from flask import Flask, jsonify
 app = Flask(__name__)
 @app.route('/')
 def home():
-    return "Hello, World!"
+    return jsonify(message="Hello, World!")
 if __name__ == '__main__':
-    app.run(port=5000, debug=True)
+    app.run(debug=True)
     
